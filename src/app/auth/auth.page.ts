c27d5e3f48aecf80af7cc22d9dc3cbc5537856c4
Marli <marleysumarly@gmail.com>
2023-08-01 03:09:34 +0700

    Add new auth module for authentication operations
    The commit adds a new auth module to the application. This module includes routing configuration for auth page, an auth.page template with all the necessary fields, and a style sheet. The TypeScript file for the auth page includes logic for switching between Login and Register state, validators, and methods for Login and Register actions. These changes are made to support user authentication through a secure form.

---
+++ src/app/auth/auth.page.ts
@@ -0,0 +1,82 @@
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.page.html',
+  styleUrls: ['./auth.page.scss'],
+})
+export class AuthPage implements OnInit {
+  isLogin = true;
+  formGroup: FormGroup;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+    // Login
+    this.formGroup = new FormGroup<any>({
+      email: new FormControl(null, {
+        updateOn: "change",
+        validators: [
+          Validators.required,
+          Validators.email
+        ]
+      }),
+      password: new FormControl(null, {
+        updateOn: "change",
+        validators: [
+          Validators.required,
+          Validators.minLength(8)
+        ]
+      })
+    });
+  }
+
+  onSwitchMode() {
+    this.isLogin = !this.isLogin;
+    this.formGroup.reset();
+    if (!this.isLogin) {
+
+      // Register
+      this.formGroup.registerControl('name', new FormControl(null, {
+        updateOn: "change",
+        validators: [
+          Validators.required,
+          Validators.minLength(3)
+        ]
+      }));
+      this.formGroup.registerControl('confirmPassword', new FormControl(null, {
+        updateOn: "change",
+        validators: [
+          Validators.required,
+          this.matchValues('password')
+        ]
+      }));
+    } else {
+
+      // Remove
+      this.formGroup.removeControl('name');
+      this.formGroup.removeControl('confirmPassword');
+    }
+  }
+
+  matchValues(matchTo: string) {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      const matchToControl = control.parent?.get(matchTo);
+
+      if (matchToControl && matchToControl.value !== control.value) {
+        return { 'notSame': true };
+      }
+
+      return null;
+    };
+  }
+
+  onLogin() {
+  }
+
+  onRegister() {
+  }
+}
