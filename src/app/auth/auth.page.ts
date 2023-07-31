eca06ce0c785ad266476f338a0564f21767b5844
Marli <marleysumarly@gmail.com>
2023-08-01 03:23:10 +0700

    Refactor auth.page.ts to remove unused import

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
