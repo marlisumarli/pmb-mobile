95a5577a9d8fbf25491e1aa97b49f8d6bf3ac129
Marli <marleysumarly@gmail.com>
2023-08-04 01:21:56 +0700

    Commit

--- src/app/students.service.ts
+++ src/app/students.service.ts
@@ -7,7 +7,7 @@ export class StudentsService {
 
   getStudents() {
     const localStorages = localStorage.getItem('students');
-    console.log(22)
+    console.log(123)
     return localStorages ? JSON.parse(localStorages) : [];
   }
 
